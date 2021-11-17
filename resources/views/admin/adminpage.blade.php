7aa1fead83011e61b0aa1931874b75b9caa4e07f
Olim-Jabborov <83329358+Olim-Jabborov@users.noreply.github.com>
2021-11-17 16:34:58 +0500

    Laravel project

---
+++ resources/views/admin/adminpage.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+
+</x-app-layout>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    @include("admin.admincss")
+  </head>
+  <body>
+
+    @include("admin.navbar")
+    
+    <!-- container-scroller -->
+    @include("admin.adminscript")
+  </body>
+</html>
